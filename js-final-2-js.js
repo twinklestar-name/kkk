d05595aab20dfd24db63bbbc27caa15345dd76b2
twinklestar-name <53613569+twinklestar-name@users.noreply.github.com>
2019-09-17 22:26:42 +0530

    Update js-final-2-js.js

--- js-final-2-js.js
+++ js-final-2-js.js
@@ -9,83 +9,118 @@ console.log("js-2-loaded")
         <h2 class="head">description</h2>
         <div id="desc">bla bla</div>
         <h2 class="head">product-preview</h2>
+        <img class="poses" src=""/>
+        <img  class="poses" src=""/>
+        <img  class="poses" src=""/>
         <a href="/nikita3/niki4/slick-demo/js-final-3-html.html"><button id="cart-btn">Add to Cart</button></a>
     </aside>
     </div>
   </div>*/
 
-  function detailItems(id,imageval,name,brand,price,descrip,photos){
+  var count=0;
+  document.body.addEventListener("click",function(event){
+    
+      if(event.target.className=="cart-btn")
+      {
+      window.localStorage.setItem("id",event.target.id)
+      var idNo=window.localStorage.getItem("id")
+      console.log(window.localStorage.setItem("itemprice",event.target.price));
+      console.log(idNo);
+      count++;
+      botn.innerHTML=count;
+      window.localStorage.setItem("cart-count",botn.innerHTML)
+      }
+      else
+      {
+      count=count+0;
+      }
+    
+  })
+  var botn=document.getElementById('cart-counting')
+  console.log(botn.innerHTML)
+  console.log(botn)
 
-  var pageWrapper=document.createElement('div');
+  function detailItems(array){
 
-  var section=document.createElement('section')
-  pageWrapper.appendChild(section)
+    var pageWrapper=document.createElement('div');
+  
+    var section=document.createElement('section')
+    pageWrapper.appendChild(section)
+  
+    var pdtImage=document.createElement('img')
+    pdtImage.id="detail-img";
+    pdtImage.src=array.preview;
+    pdtImage.setAttribute("width","300px");
+    pdtImage.setAttribute("height","400px");
+  
+    section.appendChild(pdtImage);
+  
+    var pdtDesc=document.createElement('aside')
+    pdtDesc.id="pdt-desc";
+    pageWrapper.appendChild(pdtDesc)
+  
+    var pdtName=document.createElement('h1')
+    pdtName.className="pdt-name";
+    
+    var pdtTextnode=document.createTextNode(array.name)
+    pdtName.appendChild(pdtTextnode)
+    
+    var pdtBrand=document.createElement('h2')
+    pdtBrand.className="pdt-brand";
+    
+    var pdtBrandTextnode=document.createTextNode('Brand: '+array.brand)
+    pdtBrand.appendChild(pdtBrandTextnode)
+    
+    var pdtPrice=document.createElement('div')
+    pdtPrice.className="head";
+    
+    var priceValue=document.createTextNode('price: '+array.price);
+    pdtPrice.appendChild(priceValue);
+    
+    var description=document.createElement('h2')
+    description.className="head";
+    
+    var descriptionTextnode=document.createTextNode('description')
+    description.appendChild(descriptionTextnode)
+    
+    var desc=document.createElement('div')
+    desc.id="desc";
+    var descTextnode=document.createTextNode(array.description)
+    desc.appendChild(descTextnode)
+    
+    var pdtPreview=document.createElement('h2')
+    pdtPreview.className="head";
+    var pdtPreviewTextnode=document.createTextNode('Product Preview')
+    pdtPreview.appendChild(pdtPreviewTextnode)
+    
+    var btn=document.createElement('button')
+    btn.className="cart-btn";
+    btn.id=array.id;
+    var btnTextnode=document.createTextNode('Add to Cart')
+    btn.appendChild(btnTextnode)
 
-  var pdtImage=document.createElement('img')
-  pdtImage.src=imageval;
-  pdtImage.setAttribute("width","300px");
-  pdtImage.setAttribute("height","400px");
-
-  section.appendChild(pdtImage);
-
-  var pdtDesc=document.createElement('aside')
-  pdtDesc.id="pdt-desc";
-  pageWrapper.appendChild(pdtDesc)
-
-  var pdtName=document.createElement('h1')
-  pdtName.className="pdt-name";
+    pdtDesc.appendChild(pdtName)
+    pdtDesc.appendChild(pdtBrand)
+    pdtDesc.appendChild(pdtPrice)
+    pdtDesc.appendChild(description)
+    pdtDesc.appendChild(desc)
+    pdtDesc.appendChild(pdtPreview)
+    for(var j=0;j<array.photos.length;j++)
+    {
+      var pose1=document.createElement('img')
+      pose1.className="poses";
+      pose1.src=array.photos[j];
+      pose1.setAttribute("width","80px")
+      pose1.setAttribute("height","100px")
+      pdtDesc.appendChild(pose1)
+    }
+    document.body.addEventListener("click",function(event){
+       pdtImage.src=event.target.src;
+    })
+    pdtDesc.appendChild(btn)
+    return pageWrapper;
+    }
   
-  var pdtTextnode=document.createTextNode(name)
-  pdtName.appendChild(pdtTextnode)
-  
-  var pdtBrand=document.createElement('h2')
-  pdtBrand.className="pdt-brand";
-  
-  var pdtBrandTextnode=document.createTextNode(brand)
-  pdtBrand.appendChild(pdtBrandTextnode)
-  
-  var pdtPrice=document.createElement('div')
-  pdtPrice.className="head";
-  
-  var priceValue=document.createTextNode('price: '+price);
-  pdtPrice.appendChild(priceValue);
-  
-  var description=document.createElement('h2')
-  description.className="head";
-  
-  var descriptionTextnode=document.createTextNode('description')
-  description.appendChild(descriptionTextnode)
-  
-  var desc=document.createElement('div')
-  desc.id="desc";
-  var descTextnode=document.createTextNode(descrip)
-  desc.appendChild(descTextnode)
-  
-  var pdtPreview=document.createElement('h2')
-  pdtPreview.className="head";
-  var pdtPreviewTextnode=document.createTextNode('product preview')
-  pdtPreview.appendChild(pdtPreviewTextnode)
-  
-  var anchor=document.createElement('a')
-  anchor.href="/nikita3/niki4/slick-demo/js-final-3-html.html";
-  
-  var button=document.createElement('button')
-  button.id="cart-btn";
-  var btnTextnode=document.createTextNode('Add to Cart')
-  button.appendChild(btnTextnode)
-  
-  anchor.appendChild(button)
-  pdtDesc.appendChild(pdtName)
-  pdtDesc.appendChild(pdtBrand)
-  pdtDesc.appendChild(pdtPrice)
-  pdtDesc.appendChild(description)
-  pdtDesc.appendChild(desc)
-  pdtDesc.appendChild(pdtPreview)
-  pdtDesc.appendChild(anchor)
-
-  return pageWrapper;
-  
-  }
 
   var detailCard=document.getElementById('page-2')
   function getobjects(){
@@ -98,19 +133,23 @@ console.log("js-2-loaded")
         {
           console.log('call is successful')
           var array=JSON.parse(this.responseText);
-          
-             // for(var i=0;i<array.length;i++)
-              //{
-              //console.log(array[0].preview)
-              //var cards=;
-             // console.log(cards)
-              detailCard.appendChild(detailItems(array[0].id,array[0].preview,array[0].name,array[0].brand,array[0].price,array[0].description,array[0].photos[5]));
-              //}
+          var pic,pics;
+              for(var i=0;i<array.length;i++)
+              {
+                if(array[i].id==window.localStorage.getItem("id"))
+               {
+                  detailCard.appendChild(detailItems(array[i]));
+               }
+              else
+               {
+                console.log('wrong clicked')
+               }
+              }
         }
         else
-        {
-          console.log('call failed')
-        }
+          {
+            console.log('call failed')
+          }
       }
     }
     httpRequest.open('GET','https://5d76bf96515d1a0014085cf9.mockapi.io/product',true)
